516a79a44d0b6b6b02f9cc976465542cbb5a5035
MostafaIn <mostafa.hazareh@integrify.io>
2019-10-21 21:55:04 +0300

    email validattion

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,42 @@
 import React from 'react';
-import { CssBaseline, makeStyles } from '@material-ui/core'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { CssBaseline, makeStyles } from '@material-ui/core';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
 
-import myBG from './Images/mina.jpg'
-import myBG2 from './Images/myBG2.jpg'
+import myBG from './Images/mina.jpg';
+import myBG2 from './Images/myBG2.jpg';
 
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-        height: '100%',
-        background: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${myBG}) center/cover fixed no-repeat`,
-        [theme.breakpoints.down('sm')]: {
-            background: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${myBG2}) center/cover fixed no-repeat`,
-        },
-    }
-}))
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+		height: '100%',
+		background: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${myBG}) center/cover fixed no-repeat`,
+		[theme.breakpoints.down('sm')]: {
+			background: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(${myBG2}) center/cover fixed no-repeat`,
+		},
+	},
+}));
 
 const App = () => {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <BrowserRouter>
-                <NavBar />
-                <Switch>
-                    <Route path="/" component={Home} exact />
-                    <Route path="/projects" component={Projects} />
-                    <Route path="/about" component={About} />
-                    <Route path="/contact" component={Contact} />
-                </Switch>
-            </BrowserRouter>
-        </div>
-    );
-}
+	const classes = useStyles();
+	return (
+		<div className={classes.root}>
+			<CssBaseline />
+			<BrowserRouter>
+				<NavBar />
+				<Switch>
+					<Route path="/" component={Home} exact />
+					<Route path="/projects" component={Projects} />
+					<Route path="/about" component={About} />
+					<Route path="/contact" component={Contact} />
+				</Switch>
+			</BrowserRouter>
+		</div>
+	);
+};
 
 export default App;
